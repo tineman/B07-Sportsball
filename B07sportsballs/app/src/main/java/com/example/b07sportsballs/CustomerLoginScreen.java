4e7957472812300173cd1198fc56619ff72dfa60
FLAME FIRE <xaustin03@gmail.com>
2022-07-30 16:10:13 -0400

    fixed compatibility for different screen sizes

--- B07sportsballs/app/src/main/java/com/example/b07sportsballs/CustomerLoginScreen.java
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/CustomerLoginScreen.java
@@ -1,19 +1,28 @@
 package com.example.b07sportsballs;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class CustomerLoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_customerloginscreen);
 
         Log.i("MainActivity", "Startup");
         Log.i("MainActivity", "Use \"https://b07sportsballs-default-rtdb.firebaseio.com/\"");
 
+    }
+
+    /**
+     * This method is called when the "Quit" button is pressed
+     * @param view The View object in consideration
+     */
+    public void quitButton(View view) {
+        this.finishAffinity();
     }
 }
