37b6f7a38f0531daa45aa1541c7e79607107e4e0
Tineman <stannumman50@gmail.com>
2022-07-30 12:09:48 -0400

    Use ListView and an ArrayAdapter

--- B07sportsballs/app/src/main/java/com/example/b07sportsballs/VenueScreen.java
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/VenueScreen.java
@@ -3,60 +3,49 @@ package com.example.b07sportsballs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class VenueScreen extends AppCompatActivity {
 
-    ScrollView scrollView;
+    ListView listView;
     LinearLayout linearLayout;
+    ArrayAdapter arrayAdapter;
 
     //Placeholder
-    ArrayList<String> Venues = new ArrayList<String>();
+    ArrayList<String> venues = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_screen);
 
-        //Creating ScrollView
-        scrollView = (ScrollView) findViewById(R.id.listofvenues);
+        listView = (ListView) findViewById(R.id.listofvenues);
 
-        //Creating LinearLayout. All new views should go into this layout since ScrollView can only have one child
-        linearLayout = new LinearLayout(this);
-        setContentView(linearLayout);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        arrayAdapter = new ArrayAdapter(VenueScreen.this, android.R.layout.simple_list_item_1, venues);
 
-        //To implement clickable text I'll probably add an onClickListener
+        venues.add("blargghhhh!");
+        venues.add("yarghhhh!");
+        venues.add("bleughhhh!");
+        venues.add("blargghhhh!");
+        venues.add("yarghhhh!");
+        venues.add("bleughhhh!");
+        venues.add("blargghhhh!");
+        venues.add("yarghhhh!");
+        venues.add("bleughhhh!");
+        venues.add("blargghhhh!");
+        venues.add("yarghhhh!");
+        venues.add("bleughhhh!");
+        venues.add("blargghhhh!");
+        venues.add("yarghhhh!");
+        venues.add("bleughhhh!");
 
-        //Placeholder
-        Venues.add("blargghhhh!");
-        Venues.add("yarghhhh!");
-        Venues.add("bleughhhh!");
-        Venues.add("blargghhhh!");
-        Venues.add("yarghhhh!");
-        Venues.add("bleughhhh!");
-        Venues.add("blargghhhh!");
-        Venues.add("yarghhhh!");
-        Venues.add("bleughhhh!");
-        Venues.add("blargghhhh!");
-        Venues.add("yarghhhh!");
-        Venues.add("bleughhhh!");
-        Venues.add("blargghhhh!");
-        Venues.add("yarghhhh!");
-        Venues.add("bleughhhh!");
+        listView.setAdapter(arrayAdapter);
 
-        for(String venue : Venues)
-        {
-            TextView textview = new TextView(this);
-            textview.setText(venue);
-            linearLayout.addView(textview);
-        }
 
     }
 }
