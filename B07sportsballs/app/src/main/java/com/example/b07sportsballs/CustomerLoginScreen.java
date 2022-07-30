5341c84822dee51dce483e390cea84aaad1c073f
FLAME FIRE <xaustin03@gmail.com>
2022-07-30 19:40:42 -0400

    commit CustomerSignUpScreen

--- B07sportsballs/app/src/main/java/com/example/b07sportsballs/CustomerLoginScreen.java
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/CustomerLoginScreen.java
@@ -1,28 +1,42 @@
 package com.example.b07sportsballs;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+import android.view.*;
 
 public class CustomerLoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customerloginscreen);
+
+        // These two lines of code below will remove the Title Bar From the Activity in Android
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_customer_login_screen);
 
         Log.i("MainActivity", "Startup");
         Log.i("MainActivity", "Use \"https://b07sportsballs-default-rtdb.firebaseio.com/\"");
+    }
 
+    /**
+     * This method is called when the "Register" button is pressed
+     * @param view The View object that is being considered
+     */
+    public void displayCustomerSignUpScreen(View view) {
+        Intent intent = new Intent(this, CustomerSignUpScreen.class);
+        startActivity(intent);
     }
 
     /**
      * This method is called when the "Quit" button is pressed
-     * @param view The View object in consideration
+     * @param view The View object that is being considered
      */
-    public void quitButton(View view) {
+    public void quitApp(View view) {
         this.finishAffinity();
     }
 }
