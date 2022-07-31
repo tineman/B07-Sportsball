391c5fe706bd7b49a0cc99c6884778f5e8907a25
FLAME FIRE <xaustin03@gmail.com>
2022-07-30 20:18:14 -0400

    Changed "DarkActionBar" to "NoActionBar" in themes.xml

--- B07sportsballs/app/src/main/java/com/example/b07sportsballs/CustomerSignUpScreen.java
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/CustomerSignUpScreen.java
@@ -1,7 +1,6 @@
 package com.example.b07sportsballs;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -11,11 +10,6 @@ public class CustomerSignUpScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // These two lines of code below will remove the Title Bar From the Activity in Android
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_customer_sign_up_screen);
     }
 
