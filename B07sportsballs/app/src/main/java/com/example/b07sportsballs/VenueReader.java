babf54b3105bcae8777abf60a348c64e0eae5232
Swetha <74365171+swepon@users.noreply.github.com>
2022-08-01 22:39:40 -0400

    Create Venue class
    - implement Venue class
    - create empty classes for VenueReader and VenueWriter

---
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/VenueReader.java
@@ -0,0 +1,13 @@
+package com.example.b07sportsballs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class VenueReader extends AppCompatActivity
+{
+    public void read(DatabaseReference ref)
+    {
+
+    }
+}
---
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/VenueWriter.java
@@ -0,0 +1,14 @@
+package com.example.b07sportsballs;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class VenueWriter extends AppCompatActivity
+{
+    public void write(DatabaseReference ref)
+    {
+
+    }
+}
