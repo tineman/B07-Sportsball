a36eac22a66b282d003afda806717138566325a8
FLAME FIRE <xaustin03@gmail.com>
2022-08-02 23:05:56 -0400

    Added Layout for CreateVenueScreen

---
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/CreateVenueScreen.java
@@ -0,0 +1,14 @@
+package com.example.b07sportsballs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateVenueScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_venue_screen);
+    }
+}
