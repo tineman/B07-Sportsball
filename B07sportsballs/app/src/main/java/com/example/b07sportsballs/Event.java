3f0a0b188569fe304b293b79bb75ef83a2d4c866
TIan Yu <44347881+tineman@users.noreply.github.com>
2022-08-07 16:29:15 -0400

    Add Equals and Hashcode

--- B07sportsballs/app/src/main/java/com/example/b07sportsballs/Event.java
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/Event.java
@@ -99,6 +99,18 @@ public class Event {
 
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return name.equals(event.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 
 
 
