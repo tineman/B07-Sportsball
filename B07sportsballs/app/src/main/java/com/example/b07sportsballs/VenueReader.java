8801e004d2fd6c51b7c502bb14b05805d93d7ebd
Swetha <74365171+swepon@users.noreply.github.com>
2022-08-06 22:30:58 -0400

    Update VenueReader and getAllVenues()

--- B07sportsballs/app/src/main/java/com/example/b07sportsballs/VenueReader.java
+++ B07sportsballs/app/src/main/java/com/example/b07sportsballs/VenueReader.java
@@ -3,8 +3,6 @@ package com.example.b07sportsballs;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -12,60 +10,58 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashSet;
 
-public class VenueReader extends AppCompatActivity
-{
+public class VenueReader extends AppCompatActivity {
     public static HashSet<String> keys = new HashSet<String>();
+    public static DatabaseReference ref;
+    public static boolean isRunning = true;
+    public VenueReader() {}
 
-    public VenueReader()
-    {
-
-    }
-    public void read(DatabaseReference ref)
-    {
-        ref = ref.child("Root").child("Venues");
-
-        ref.addValueEventListener(new ValueEventListener()
-          {
-              @Override
-              public void onDataChange(DataSnapshot dataSnapshot)
-              {
-                  HashSet<String> venues = new HashSet<String>();
-                  Log.i("demo", "data changed");
-                  for (DataSnapshot child : dataSnapshot.getChildren()) {
-                      String key = child.getKey();
-                      Log.i("demo", "Key: " +key);
-                      venues.add(key);
-                  }
-                  updateKeys(venues);
-              }
-
-              @Override
-              public void onCancelled(DatabaseError databaseError)
-              {
-                  Log.w("VenueReader warning", "Error while reading venues from database",
-                          databaseError.toException());
-              }
-          }
-
-        );
-
-
-    }
-    public void updateKeys(HashSet<String> venues)
-    {
-        for (String key: venues)
-        {
-            if (!keys.contains(key))
-            {
-                keys.add(key);
+    public void readData(FirebaseCallback myCallback) {
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Log.i("demo", "data changed");
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    String key = child.getKey();
+                    keys.add(key);
+                }
+                myCallback.onCallback(keys);
             }
-        }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("VenueReader warning", "Error while reading venues from database",
+                        databaseError.toException());
+            }
+
+        });
     }
+
+    public void read(DatabaseReference ref) {
+        readData(new FirebaseCallback(){
+            @Override
+            public void onCallback(HashSet<String> keys) {
+                Venue.allVenues = keys;
+                isRunning = false;
+                Venue v = new Venue();
+                v.getAllVenues();
+            }
+        });
+    }
+
+
 
 
 
 
 }
+
+
+
+
+
+
+
 
 
 
